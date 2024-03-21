6aabcd58e7623d15344e19951b8a95e94ff8459b
Gabriel Tobing <6481213l@gmail.com>
2024-03-21 17:18:22 +1100

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,20 @@
+# Micromouse - Comsoc x LTU Robotics club Prototype
+
+def move_left():
+    # Moves the mouse to the left
+    return
+
+def move_right():
+    # Moves the mouse to the right
+    return
+
+def move_forward():
+    # Moves forward 1 unit
+    return
+
+def map_initialisation():
+    
+
+# Main code section
+
+# A* search algorithm
