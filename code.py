c3d20763c9bf68f35a166f4c4bd6415127ef0fc1
Gabriel Tobing <6481213l@gmail.com>
2024-03-21 18:48:54 +1100

    Create code.py

---
+++ code.py
@@ -0,0 +1,23 @@
+def motor_control():
+    # motor control (PIDs)
+    return
+
+def odometry():
+    # track position and angle
+    return
+
+def check_for_walls():
+    # check what walls robot can see and put them into datastructure
+    return
+
+def maze_search():
+    # path to search through maze (incuding movement)
+    return
+
+def path_planning():
+    # best path to finish (fastest)
+    return
+
+def sensor_reading():
+    # turning sensor data into usable values
+    return
