e3f1022ca1cd8de0cdf6f448a05fc31de8b8f50d
Gabriel Tobing <6481213l@gmail.com>
2024-03-21 18:14:46 +1100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 # Micromouse - Comsoc x LTU Robotics club Prototype
 
-def move_left():
+def rotate_left():
     # Moves the mouse to the left
     return
 
-def move_right():
+def rotate_right():
     # Moves the mouse to the right
     return
 
@@ -13,7 +13,17 @@ def move_forward():
     return
 
 def map_initialisation():
+    # Maps the whole play area
     
+    map_nodes = []
+
+    # Move forward, then left or right
+    # Move
+
+    []
+
+
+    return map_nodes
 
 # Main code section
 
